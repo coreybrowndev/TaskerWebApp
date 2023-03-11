e8b6b409d7cd551a1ec09076ebdf22af2e69e8a9
Corey <corey.brown@bsu.edu>
2023-03-10 21:26:27 -0500

    Inital REST API for tasks

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require("express-async-handler");
 
+const Task = require("../models/taskModel");
+
 // @desc Get tasks
 //@route GET /api/tasks
 //@access private
 const getTasks = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get tasks" });
+  const tasks = await Task.find();
+  res.status(200).json(tasks);
 });
 
 // @desc New task
@@ -16,21 +19,42 @@ const createTask = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text field");
   }
-  res.status(200).json({ message: "Created task" });
+  const task = await Task.create({
+    text: req.body.text,
+  });
+  res.status(200).json(task);
 });
 
 // @desc Update tasks
 //@route PUT /api/tasks
 //@access private
 const updateTask = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update task ${req.params.id}` });
+  const task = await Task.findById(req.params.id);
+
+  if (!task) {
+    res.status(400);
+    throw new Error("Task not found");
+  }
+
+  const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedTask);
 });
 
 // @desc Delete tasks
 //@route DELETE /api/tasks
 //@access private
 const deleteTask = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete task ${req.params.id}` });
+  const deleteTask = await Task.findByIdAndDelete(req.params.id);
+
+  if (!deleteTask) {
+    res.status(400);
+    throw new Error("Task not found");
+  }
+
+  res.status(200).json(req.params.id);
 });
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5500;
 const { errorHandler } = require("./middleware/errorMiddleware");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 
